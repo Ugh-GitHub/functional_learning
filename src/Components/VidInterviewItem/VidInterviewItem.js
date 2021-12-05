afa21637a7e7a8ba77e1ffbfcea7d04baecf59ee
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-12-05 16:47:35 -0600

    converted VidInterviewItem to work off of object arrays

--- src/Components/VidInterviewItem/VidInterviewItem.js
+++ src/Components/VidInterviewItem/VidInterviewItem.js
@@ -5,29 +5,28 @@ import { Accordion } from 'react-bootstrap';
 
 export default function VidInterviewItem() {
     let AccordionItems = [
-        {key: 0, question: "Why should we hire you?", link:'https://www.youtube.com/embed/E7wJTI-1dvQ'},
-        {key: 0, question: "What are some other skills we should know about?", link:'https://www.youtube.com/watch?v=LYG7sMtb6u8'}]
+        {question: "Why should we hire you?", link:'https://www.youtube.com/embed/E7wJTI-1dvQ'},
+        {question: "What are some other skills we should know about?", link:'https://www.youtube.com/embed/LYG7sMtb6u8'}]
   
     return (
 
-        
-                <Accordion.Item eventKey="0"> 
-                {/* Add in the number here */}
-                    <Accordion.Header>Dynamic Header</Accordion.Header> 
-                    {/* Add in the Interview question here */}
-                    <Accordion.Body>
-                        <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
-                            //Youtube link fed here
-                            frameborder='50'
-                            allow='autoplay; encrypted-media'
-                            allowfullscreen
-                            title='video'
-                            width="600"
-                            height="300"
-                        />
-                    </Accordion.Body>
-                </Accordion.Item>
+        <>
+                {AccordionItems
+                    .map((interview, index) => (
+                        <Accordion.Item eventKey={index}> 
+                            <Accordion.Header>{interview.question}</Accordion.Header> 
+                            <Accordion.Body>
+                                <iframe src={interview.link}
+                                    frameborder='50'
+                                    allow='autoplay; encrypted-media'
+                                    allowfullscreen
+                                    title='video'
+                                    width="600"
+                                    height="300"
+                                />
+                            </Accordion.Body>
+                        </Accordion.Item>))}
             
-
+        </>
     );
 }
