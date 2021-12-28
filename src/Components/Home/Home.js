424f995d4213482a19e0e7f42a256d6963009386
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-12-28 10:03:20 -0600

    added alt tags for images

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="homeFormat">
         <div className="sectionDivides">
-          <img src="/images/WordPress_Headshot.jpeg" className="headshot" ></img>
+          <img src="/images/WordPress_Headshot.jpeg" className="headshot" alt="Peter Headshot"></img>
           <h1 className="aboutHeader">Welcome!</h1>
           <p className="aboutSummary">
             Accomplished Account Manager with over 6 years experience in managing and solutioning for two $3-5 million accounts. 
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -9,10 +9,10 @@ export default function Resume() {
                 <span>Peter Pierce <br/></span>
                 <span>(952) 491 - 0151 <br/></span>
                 <a href="https://www.linkedin.com/in/peter-p-2b757737">
-                    <img src="/images/LI-In-Bug.png" className="logoIcon" ></img>
+                    <img src="/images/LI-In-Bug.png" className="logoIcon" alt="Linkedin Logo"></img>
                 </a>
                 <a href="https://github.com/Ugh-GitHub">
-                    <img src="/images/GitHub-Mark-64px.png" className="logoIcon" ></img>
+                    <img src="/images/GitHub-Mark-64px.png" className="logoIcon" alt="GitHub Logo"></img>
                 </a>
             </div>
             
