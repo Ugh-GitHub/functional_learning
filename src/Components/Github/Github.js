d4099ee1788090d2e13379c1eb05f39aae2a512b
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-01-17 12:29:13 -0600

    added code for ApolloClient tutorial

--- src/Components/Github/Github.js
+++ src/Components/Github/Github.js
@@ -1,39 +1,76 @@
-async function getContributions(token, username) {
-  const headers = {
-      'Authorization': `bearer ${token}`,
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
   }
-  const body = {
-      "query": `query {
-          user(login: "${username}") {
-            name
-            contributionsCollection {
-              contributionCalendar {
-                colors
-                totalContributions
-                weeks {
-                  contributionDays {
-                    color
-                    contributionCount
-                    date
-                    weekday
-                  }
-                  firstDay
-                }
-              }
-            }
-          }
-        }`
-  }
-  const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers });
-  const data = await response.json();
-  return data;
+`;
+
+function ExchangeRates() {
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
 }
 
-const githubData = (async () => {
-  const data = await getContributions(process.env.REACT_APP_GITHUB_CONTRIBUTIONS_READ_TOKEN, 'ugh-github');
-  const contributions = data.data.user.contributionsCollection.contributionCalendar;
-  console.log(contributions);
-  return contributions;
-})();
+export default ExchangeRates;
 
-export default githubData;
+
+
+// async function getContributions(token, username) {
+//   const headers = {
+//       'Authorization': `bearer ${token}`,
+//   }
+//   const body = {
+//       "query": `query {
+//           user(login: "${username}") {
+//             name
+//             contributionsCollection {
+//               contributionCalendar {
+//                 colors
+//                 totalContributions
+//                 weeks {
+//                   contributionDays {
+//                     color
+//                     contributionCount
+//                     date
+//                     weekday
+//                   }
+//                   firstDay
+//                 }
+//               }
+//             }
+//           }
+//         }`
+//   }
+//   const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers });
+//   const data = await response.json();
+//   return data;
+// }
+
+// const githubData = (async () => {
+//   const data = await getContributions(process.env.REACT_APP_GITHUB_CONTRIBUTIONS_READ_TOKEN, 'ugh-github');
+//   const contributions = data.data.user.contributionsCollection.contributionCalendar;
+//   console.log(contributions);
+//   return contributions;
+// })();
+
+// export default githubData;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 import './index.css';
 import App from './Components/App/App';
 // import { store } from './Components/counter/store';
@@ -13,46 +20,66 @@ import Invoice from './Components/InvoiceItem/InvoiceItem';
 import Test from './Components/VideoInterview/VideoInterview';
 import Resume from './Components/Resume/Resume';
 import Home from './Components/Home/Home';
-import Github from './Components/Github/Github';
+import ExchangeRates from './Components/Github/Github';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 
 ReactDOM.render(
-  <Router>
-    {/* <React.StrictMode>
-      <Provider store={store}> */}
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="github" element={<Github/>}/>
-          <Route index element={<Home />}/>
-          <Route path="interviews" element={<Test />}/>
-          <Route path="resume" element={<Resume />} />
-          <Route path="projects" element={<Expenses />} />
-          <Route path="portfolio" element={<Portfolio />}>
-            {/* Default on page load */}
+  <ApolloProvider client={client}>
+    <Router>
+      {/* <React.StrictMode>
+        <Provider store={store}> */}
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="github" element={<ExchangeRates/>}/>
+            <Route index element={<Home />}/>
+            <Route path="interviews" element={<Test />}/>
+            <Route path="resume" element={<Resume />} />
+            <Route path="projects" element={<Expenses />} />
+            <Route path="portfolio" element={<Portfolio />}>
+              {/* Default on page load */}
+              <Route
+                index
+                element={
+                  <main style={{ padding: '1rem' }}>
+                    <p>Select an invoice</p>
+                  </main>
+                }
+              />
+              {/*  */}
+              <Route path=":invoiceId" element={<Invoice />} />
+            </Route>
             <Route
-              index
+              path="*"
               element={
                 <main style={{ padding: '1rem' }}>
-                  <p>Select an invoice</p>
+                  <p>There's nothing here!</p>
                 </main>
               }
             />
-            {/*  */}
-            <Route path=":invoiceId" element={<Invoice />} />
           </Route>
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: '1rem' }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Route>
-    </Routes>
-      {/* </Provider>
-    </React.StrictMode> */}
-  </Router>,
-  document.getElementById('root')
+      </Routes>
+        {/* </Provider>
+      </React.StrictMode> */}
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
