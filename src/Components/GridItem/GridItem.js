510a6184cc82689f4e7ec2b94ba1d05da3a44e1f
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-01-19 12:39:06 -0600

    added object fit to portfolio images for a basic display

--- src/Components/GridItem/GridItem.js
+++ src/Components/GridItem/GridItem.js
@@ -4,7 +4,7 @@ export default function GridItem({ project }) {
     return (
 
         <>
-            <img src={project.imageLink} className="PortfolioCard" alt={project.altTag}></img>
+            <img src={project.imageLink} className="PortfolioCardImage" alt={project.altTag}></img>
             <h4><b>{project.name}</b></h4>
             <p>{project.due}</p>
             <p>Text goes here</p>
